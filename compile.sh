50561ccff292aa0e74ca01dbb1d3d40410b068ff
Klevis Imeri <klevisimeri11@gmail.com>
2026-04-23 01:01:03 +0200

    [Update] Updated the template

---
+++ compile.sh
@@ -0,0 +1,1 @@
+rustc -A warnings E.rs -o sol && RUST_BACKTRACE=1 ./sol < in.txt
--- temp.rs
+++ temp.rs
@@ -1,19 +1,23 @@
-use std::io::{stdin, stdout, BufWriter, Read, Write};
+use std::{cmp::*, collections::*, io::*, mem::*,};
+
 fn main() {
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
     // let mut scan = stdin().lines();
     let mut scan = input.split_ascii_whitespace();
-    macro_rules! next { ()=>{scan.next().unwrap().parse().unwrap()}; }
-    // macro_rules! next { ()=>{scan.next().unwrap().unwrap().parse().unwrap()}; }
+    macro_rules! next { ()=>{scan.next().unwrap()}; }
+    // macro_rules! next { ()=>{scan.next().unwrap().unwrap()}; }
+    macro_rules! nextp { ()=>{next!().parse().unwrap()}; }
+    macro_rules! nextstr { ()=>{next!().as_bytes().to_vec()}; }
     let mut out = BufWriter::new(stdout().lock());
     macro_rules! put { ($($arg:tt)*) => { write!(out, $($arg)*).unwrap() }; }
     // macro_rules! put { ($($arg:tt)*) => { write!(out, $($arg)*).unwrap(); out.flush().unwrap() }; }
+    macro_rules! dbg { ($($arg:tt)*) => {}; }
 
+    let t: i64 = nextp!(); dbg!(t);
     let mut solve = || {
 
     };
 
-    let t: i64 = 1; for _ in 0..t { solve(); }
-    // solve();
+    for _ in 0..t { solve(); }
 }
